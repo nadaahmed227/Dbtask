e03cba356779f196dc4403101466db2eeea1732c
nada ahmed <na5822192@gmail.com>
2023-01-25 21:28:04 +0200

    add buttm reset

--- Dbtask/Form1.Designer.cs
+++ Dbtask/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -75,7 +76,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(340, 400);
+            this.textBox1.Location = new System.Drawing.Point(340, 385);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(316, 30);
             this.textBox1.TabIndex = 3;
@@ -105,11 +106,12 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Teal;
-            this.label3.Location = new System.Drawing.Point(335, 353);
+            this.label3.Location = new System.Drawing.Point(335, 354);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(94, 28);
             this.label3.TabIndex = 5;
             this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button1
             // 
@@ -123,12 +125,25 @@
             this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // Reset
+            // 
+            this.Reset.AutoSize = true;
+            this.Reset.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.ForeColor = System.Drawing.Color.Teal;
+            this.Reset.Location = new System.Drawing.Point(445, 552);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(59, 28);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "Reset";
+            this.Reset.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(781, 665);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.Reset);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
@@ -158,6 +173,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Reset;
     }
 }
 
--- Dbtask/Form1.cs
+++ Dbtask/Form1.cs
@@ -31,5 +31,10 @@ namespace Dbtask
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
