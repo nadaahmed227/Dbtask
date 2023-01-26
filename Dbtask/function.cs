2c9e569b8dd6840e01f45095afde248bc626ce64
nada ahmed <na5822192@gmail.com>
2023-01-26 08:22:49 +0200

    add SqlConnection

--- Dbtask/function.cs
+++ Dbtask/function.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace Dbtask
 {
     internal class function
     {
+        private SqlConnection Con; 
+        private
+        public function
     }
 }
