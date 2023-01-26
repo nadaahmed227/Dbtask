509e4bfa4b7ceaaea07d7e934cfad1e49994b7b5
nada ahmed <na5822192@gmail.com>
2023-01-26 09:05:13 +0200

    add sda sqldataAdapter

--- Dbtask/function.cs
+++ Dbtask/function.cs
@@ -8,25 +8,27 @@ using System.Threading.Tasks;
 
 namespace Dbtask
 {
-    internal class function
+    class function
     {
         private SqlConnection Con;
         private SqlCommand Cmd;
         private DataTable dt;
-        private SqlDataAdapter sda; 
-        private string Constr; 
+        private SqlDataAdapter sda;
+        private string Constr;
         public functions()
         {
 
             Constr = @"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\bahgat\\source\\repos\\Dbtask\\Dbtask\\EmpDb.mdf;Integrated Security=True;Connect Timeout=30"
-            Con= new SqlConnection(Constr);
+            Con = new SqlConnection(Constr);
             Cmd = new SqlCommand();
             Cmd.Connection = Con;
         }
-        public DataTable GetData(string Query);  {
-        dt =new DataTable();  
+        public DataTable GetData(string Query);  
+        {
+        dt =new DataTable();
+        sda= new SqlDataAdapter =(Query, Constr); 
+            sda.fill(dt); 
+            return dt;  
+            }
         }
-
-    } 
-
 }
